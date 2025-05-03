3a3706535caed862408ccda0c680902d12c18fba
Chris Wen <chriswen430@gmail.com>
2025-05-03 14:57:57 +1000

    [sit737] 10.1P

---
+++ 10.1P/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const logger = require('./logger-init');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.get('/contents', (req, res) => {
+    const contents = [
+        { id: 1, title: 'Content 1', description: 'Containerize a simple application' },
+        { id: 2, title: 'Content 2', description: 'Deploy the containerized application' },
+        { id: 3, title: 'Content 3', description: 'To ensure proper monitoring and visibility of cloud-native application' },
+    ];
+    res.json(contents);
+}
+);
+
+
+app.listen(port, () => {
+    logger.info(`Server is running at http://localhost:${port}`);
+});
