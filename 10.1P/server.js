de79b2c761e5f23b64da371390d1dd4298deca1a
Chris Wen <chriswen430@gmail.com>
2025-05-06 00:09:28 +1000

    [sit737] gcp configuration.

--- 10.1P/server.js
+++ 10.1P/server.js
@@ -9,11 +9,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/contents', (req, res) => {
+    logger.info('GET /contents endpoint called');
     const contents = [
         { id: 1, title: 'Content 1', description: 'Containerize a simple application' },
         { id: 2, title: 'Content 2', description: 'Deploy the containerized application' },
         { id: 3, title: 'Content 3', description: 'To ensure proper monitoring and visibility of cloud-native application' },
     ];
+
+    logger.info('Contents retrieved successfully');
     res.json(contents);
 }
 );
