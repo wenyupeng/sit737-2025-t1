50580884e38aa47ef99171f800dc6adce00c130c
Chris Wen <chriswen430@gmail.com>
2025-05-20 16:55:12 +1000

    [sit737] fix port error.

--- mock-bank/account-service/src/server.ts
+++ mock-bank/account-service/src/server.ts
@@ -5,7 +5,7 @@ import { accountRoute } from './routes';
 
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
